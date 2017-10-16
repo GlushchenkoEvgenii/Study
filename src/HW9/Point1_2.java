cb3d34cc7119dff8b4152bbe087af2d55180f545
GlushchenkoEvgenii <merefyanin@gmail.com>
2017-10-17 00:22:12 +0300

    Test

---
+++ src/HW9/Point1_2.java
@@ -0,0 +1,13 @@
+package HW9;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Point1_2  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner("mama.txt");
+        System.out.println(scanner);
+
+    }
+
+}
