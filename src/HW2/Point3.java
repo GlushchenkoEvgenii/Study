662ff9971ac4773aee1f7638d239f2ee8e8c84d4
GlushchenkoEvgenii <merefyanin@gmail.com>
2017-10-30 20:07:39 +0200

    )))

---
+++ src/HW2/Point3.java
@@ -0,0 +1,23 @@
+package HW2;
+
+
+public class Point3 {
+    public static void main(String[] args) {
+        String[] strings= new String[12];
+        strings[0]=("Jan");
+        strings[1]=("Feb");
+        strings[2]=("Mar");
+        strings[3]=("Apr");
+        strings[4]=("May");
+        strings[5]=("Jun");
+        strings[6]=("Jul");
+        strings[7]=("Aug");
+        strings[8]=("Sep");
+        strings[9]=("Oct");
+        strings[10]=("Nov");
+        strings[11]=("Dec");
+        for (int i=0; i<12; i++) {
+            System.out.println(strings[4]);
+        }
+    }
+}
